4e5a6cb13a4944e518a7b192337ccc4a78771e41
tsegishey@yahoo.com <tsegishey@yahoo.com>
2022-09-15 18:01:58 -0500

    added dialog box and images

---
+++ SampleDuplicateFileFinder/SampleDuplicateFileFinder/Dialog/AboutDialogBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SampleDuplicateFileFinder.Utils;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SampleDuplicateFileFinder.Dialog
+{
+    /// <summary>
+    /// Interaction logic for AboutDialogBox.xaml
+    /// </summary>
+    public partial class AboutDialogBox : Window
+    {
+     public AboutDialogBox()
+        {
+            InitializeComponent();
+            BindingCommands();
+            GetVersionInfo();
+            DataContext = this;
+    }
+    public string Version { get; set; }
+    private void GetVersionInfo()
+    {
+        Version = CoreAssembly.Version.ToString(2);
+    }
+
+    private void BindingCommands()
+    {
+        CommandBindings.Add(new CommandBinding(AppCommands.Ok, (s, a) => Close()));
+    }
+
+}
+}
+
+
+
