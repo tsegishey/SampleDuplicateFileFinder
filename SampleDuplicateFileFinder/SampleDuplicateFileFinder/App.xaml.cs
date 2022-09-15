7c14964b10c1e0d1c124e5e8f6cf69d7dca9efb3
tsegishey@yahoo.com <tsegishey@yahoo.com>
2022-09-15 13:26:54 -0500

    added app error code

--- SampleDuplicateFileFinder/SampleDuplicateFileFinder/App.xaml.cs
+++ SampleDuplicateFileFinder/SampleDuplicateFileFinder/App.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace SampleDuplicateFileFinder
@@ -20,9 +15,48 @@ namespace SampleDuplicateFileFinder
         public App()
         {
             Current = this;
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(AppEventHandler);
+
+            // testing UnhandledException
+            //try
+            //{
+            //    throw new Exception("Error For testing");
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Catch clause caught : {0} \n", e.Message);
+            //    MessageBox.Show(e.Message, Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+            //throw new Exception("Big Error");
         }
 
         public new static App Current { get; private set; }
         public static string Name => Constant.AppName;
+        public static string ErrorMessage => Constant.AppErrorTerminate;
+        static void AppEventHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            string message = $"Unhandled Exception {(args.IsTerminating ? ErrorMessage : "")}";
+            if (e is Exception exception)
+            {
+                message += Environment.NewLine + "Error: ";
+                while (true)
+                {
+                    message += exception.Message;
+                    if (exception.InnerException == null)
+                        break;
+
+                    message += Environment.NewLine + "Inner: ";
+                    exception = exception.InnerException;
+                }
+                MessageBox.Show(message, Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                string msg = $"{message}.\nError Object Type:{(object?)args.ExceptionObject.GetType().FullName}\nError Object:{args.ExceptionObject}";
+                MessageBox.Show(msg, Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
--- SampleDuplicateFileFinder/SampleDuplicateFileFinder/Constant.cs
+++ SampleDuplicateFileFinder/SampleDuplicateFileFinder/Constant.cs
@@ -2,4 +2,5 @@
 internal static class Constant
 {
     public const string AppName = "Sample Duplicate File Finder";
+    public const string AppErrorTerminate = ", the application is terminating.";
 }
