8be4ea73c543d69bcb8618ddbc8eea7fffbbf058
tsegishey@yahoo.com <tsegishey@yahoo.com>
2022-09-19 21:33:13 -0500

    added grid splitter , view and view drive model

---
+++ SampleDuplicateFileFinder/SampleDuplicateFileFinder/ViewModel/DriveViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace SampleDuplicateFileFinder.ViewModel;
+
+public class DriveViewModel
+ {
+
+    public DriveViewModel(DriveInfo driveInfo)
+    {
+        DriveInfo = driveInfo;
+        Name = driveInfo.Name[..^1];
+    }
+
+    public string Name { get; protected set; }
+    public DriveInfo DriveInfo { get; private set; }
+ }
+
---
+++ SampleDuplicateFileFinder/SampleDuplicateFileFinder/Views/FindDuplicateFileView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SampleDuplicateFileFinder.ViewModel;
+
+namespace SampleDuplicateFileFinder.Views
+{
+    /// <summary>
+    /// Interaction logic for FindDuplicateFileView.xaml
+    /// </summary>
+    public partial class FindDuplicateFileView : Window
+    {
+        public FindDuplicateFileView()
+        {
+            InitializeComponent();
+            Drives = new ObservableCollection<DriveViewModel>();
+            var diskDrives = DriveInfo.GetDrives().Where(x => x.IsReady).Select(x => new DriveViewModel(x));
+            foreach(DriveViewModel drive in diskDrives)
+            {
+                Drives.Add(drive);
+            }
+            DataContext = this;
+        }
+
+        public ObservableCollection<DriveViewModel> Drives { get; set; }
+    }
+}
